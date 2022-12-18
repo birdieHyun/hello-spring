e7e96c4fe4de789605fc6ac50e59aeb6c7c98969
alpacaHyun <flaehdan@gmail.com>
2022-12-18 21:07:26 +0900

    feat : hello
    body :

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,9 +1,16 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
-    public
+    @GetMapping("hello") // 웹에서 헬로우를 클릭하면 이 메서드를 호출해준다.
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
+
+    }
 }
