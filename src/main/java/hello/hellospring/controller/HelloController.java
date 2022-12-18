b9666b3ea31d68e4bd7e462ae0205453b3b7e60f
alpacaHyun <flaehdan@gmail.com>
2022-12-19 07:51:49 +0900

    feat : MVC, API
    body :

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,30 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello") // 웹에서 헬로우를 클릭하면 이 메서드를 호출해준다.
-    public String hello(Model model){
+    public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello"; // templates 의 에서 hello 를 찾는다 -> hello.html 찾는다.
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam(value = "name", required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // http 바디 부분에 return 한 값을 직접 넣어주겠다는 의미
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+
+
 }
